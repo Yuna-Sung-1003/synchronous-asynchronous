f81c3035e5eeb46e0de39b0022995f37fb57cc16
Yuna-Sung-1003 <86541613+Yuna-Sung-1003@users.noreply.github.com>
2021-07-11 18:14:32 +0900

    callback 함수

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-function work() {
-  //setTimeout:함수내에서 하고자하는 작업이 백그라운드에서 수행되기에, 영향을 주지 않기에 동시 다른 작업 가능
+//Callback
+//Work 이후의 작업을 추가하고싶을때, callback 을 파라미터로 넘겨주기
+function work(callback) {
   setTimeout(() => {
     const start = Date.now();
     for (let i = 0; i < 1000000000; i++) {}
     const end = Date.now();
-    console.log(end - start + "ms");
-  }, 0); //0ms 이후에 실행, 4번째로 실행
+    console.log(end - start + "ms"); //4
+    callback(end - start); //얼마나 걸렸는가
+  }, 0);
 }
 
-console.log("작업시작"); //1번째로 실행
-
-work(); //0ms 이후 2번째로 실행
-console.log("다음작업"); // for 루프 도는 동안 3번째 실행
+console.log("작업시작"); //1
+work((ms) => {
+  // 2실행되면서 위 work함수가 실행,루프 돌아가는중
+  console.log("작업이 끝났어요!"); //5
+  console.log(ms + "ms 걸렸다고 해요"); //end - start(ms) 를 함수 내부에서 받아와 나타냄
+});
+console.log("다음작업"); //3
