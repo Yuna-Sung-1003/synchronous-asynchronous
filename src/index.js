0428fd62cd37a3ea7f94a2e6df270806c4054cb4
Yuna-Sung-1003 <86541613+Yuna-Sung-1003@users.noreply.github.com>
2021-07-11 19:13:43 +0900

    async, await 화살표 함수로 async 실행. 하나씩 처리.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms)); //특정 ms 가 끝난 이후에 작업끝
 }
-async function makeError() {
-  await sleep(1000);
-  const error = new Error();
-  throw error; //에러 발생 시키고 싶을때
-}
-//앞부분에 async 붙여려주기
-//에러를 잡아내고 싶을 때 try catch 문
+
+//화살표 함수로 async 만들고 싶을때, 함수 만들기
+const getDog = async () => {
+  await sleep(1000); //1초
+  return "멍멍";
+};
+const getRabbit = async () => {
+  await sleep(500); //0.5초
+  return "깡총";
+};
+const getCat = async () => {
+  await sleep(3000); //3초
+  return "야옹";
+};
+//힘수들 호출, 하나씩 처리중
 async function process() {
-  try {
-    await makeError();
-  } catch (e) {
-    //e: 에러 가르키는 객체
-    console.error(e);
-  }
+  const dog = await getDog();
+  console.log(dog);
+  const rabbit = await getRabbit();
+  console.log(rabbit);
+  const cat = await getCat();
+  console.log(cat);
 }
+
 process();
