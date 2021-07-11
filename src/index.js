e98a125f35dd3a8164dd891eddbb1af02a6bbfd3
Yuna-Sung-1003 <86541613+Yuna-Sung-1003@users.noreply.github.com>
2021-07-11 19:04:23 +0900

    error(try catch) 에러를 발생시키고 에러를 잡아내고 싶을때 ( try catch 문)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms)); //특정 ms 가 끝난 이후에 작업끝
 }
-
+async function makeError() {
+  await sleep(1000);
+  const error = new Error();
+  throw error; //에러 발생 시키고 싶을때
+}
 //앞부분에 async 붙여려주기
+//에러를 잡아내고 싶을 때 try catch 문
 async function process() {
-  console.log("안녕하세요");
-  //promise 앞에는  await
-  await sleep(1000); //1초동안 기다림
-  console.log("반가워요");
+  try {
+    await makeError();
+  } catch (e) {
+    //e: 에러 가르키는 객체
+    console.error(e);
+  }
 }
 process();
